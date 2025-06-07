edbb94d8a1169a1eedf163341c418ef2aafcde12
Lucas Maloni <l.maloni.c@gmail.com>
2025-06-07 14:52:31 -0300

    checking valid tiles to play

--- board.py
+++ board.py
@@ -1,7 +1,7 @@
 class Board:
     
     def __init__(self):
-        self.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+        self.board = [['    ', '    ', '    '], ['    ', '    ', '    '], ['    ', '    ', '    ']]
         self.board_size = len(self.board)
     
     def print_board(self):
@@ -11,11 +11,17 @@ class Board:
             print(f"{self.board[i][0]}|{self.board[i][1]}|{self.board[i][2]}")
     
             if i < self.board_size-1:
-                print("-+-+-")
+                print("----+----+----")
         print()
+    
+    def check_tile(self, row, column):
+        '''returns boolean value if the picked tile is empty'''
+        return self.board[row-1][column-1] == "    "
+
             
 #testing
 tabuleiro = Board()
 
 tabuleiro.print_board()
+print(tabuleiro.check_tile(1, 1)) #true
            
