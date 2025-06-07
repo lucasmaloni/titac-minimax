c2324e9859708469527f48da224f864dc7a08f27
Lucas Maloni <l.maloni.c@gmail.com>
2025-06-07 15:04:48 -0300

    refactoring

--- titac_minimax/board.py
+++ titac_minimax/board.py
@@ -1,22 +1,22 @@
 class Board:
     
     def __init__(self):
-        self.board = [['    ', '    ', '    '], ['    ', '    ', '    '], ['    ', '    ', '    ']]
-        self.board_size = len(self.board)
+        self.EMPTY_SPACE = '    '
+        self.board = [[self.EMPTY_SPACE for i in range(3)] for i in range(3)] 
     
     def print_board(self):
         '''prints the current board in the terminal'''
         print()
-        for i in range(self.board_size):
+        for i in range(3):
             print(f"{self.board[i][0]}|{self.board[i][1]}|{self.board[i][2]}")
     
-            if i < self.board_size-1:
+            if i < 2:
                 print("----+----+----")
         print()
     
     def check_tile(self, row, column):
         '''returns boolean value if the picked tile is empty'''
-        return self.board[row-1][column-1] == "    "
+        return self.board[row-1][column-1] == self.EMPTY_SPACE
 
             
 #testing
