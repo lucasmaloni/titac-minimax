5801cbbfcfeae723c674ef928d1a4c0c2964aac6
Lucas Maloni <l.maloni.c@gmail.com>
2025-06-08 18:07:31 -0300

    a lot of changes

--- titac_minimax/game.py
+++ titac_minimax/game.py
@@ -5,26 +5,31 @@ class Game:
     
     def __init__(self):
         self.board = Board()
-        self.players = ['X', 'O']
-        self.current_player = self.players[0]
+        self.current_player = "X"
         
     def run(self):
         '''the main structure of the game and it's turn based logic'''
         while True:
-            print(f'Player {self.current_player}, it is your turn!\nPlease inform the row and column to play:\n')
-            self.board.print_board()
             
-            while True: #player input loop
-                row = int(input("row: "))
-                column = int(input("column: "))
+            if self.current_player == "X":
+                print(f'Player X, it is your turn!\nPlease inform the row and column to play:\n')
+                self.board.print_board()
                 
-                if self.board.set_letter(self.current_player, row, column):
-                    print(f'The decision was made, player {self.current_player}!\n') #end of the turn
-                    break
-                else:
-                    print('This play is not valid, pick a new one:')
-                        
+                while True: #player input loop
+                    row = int(input("row: "))
+                    column = int(input("column: "))
                     
+                    if self.board.set_letter(self.current_player, row, column):
+                        print(f'The decision was made, player X!\n') #end of the turn
+                        break
+                    else:
+                        print('This play is not valid, pick a new one:')
+            
+            if self.current_player == "O":
+                #the machine logic will enter here - further implementation required!
+                print("now the machine must play")
+                break
+               
             self.board.print_board()
             
             if self.board.check_win(self.current_player):
@@ -39,31 +44,35 @@ class Game:
     
     def change_player(self):
         '''change current player based on current player'''
-        if self.current_player == self.players[0]:
-            self.current_player = self.players[1]
-        else:
-            self.current_player = self.players[0]     
+        if self.current_player == "X":
+            self.current_player = "O"
+            return
+        
+        if self.current_player == "O":
+            self.current_player = "X"
+            return    
     
-    def evaluate(self):
+    def evaluate(self, board):
         '''evaluates and return the current state of the game'''
-        if self.board.check_win(self.players[0]):
+        if board.check_win("X"): #human victory
             return -10
-        elif self.board.check_win(self.players[1]):
+        elif board.check_win("O"): #machine victory
             return 10
         
-        if self.board.check_draw():
+        if board.check_draw():
             return 0
 
         return
 
     def copy_board(self): #provisory
-        board_copy = copy.deepcopy(self.board.board)
+        '''we copy the board Object for further use in the minimax'''
+        board_copy = copy.deepcopy(self.board)
         return board_copy
 
-    def minimax(self):
+    def minimax(self, board: Board, max_player: bool):
         ''''''
-        if self.board.terminal():
-            return self.evaluate()
+        if board.terminal():
+            return self.evaluate(board)
 
         return
         
