bca9a2654d69f27b6f6c6c4bf1534a40ca50bd1c
Lucas Maloni <l.maloni.c@gmail.com>
2025-06-13 21:56:57 -0300

    plot tree function in progressss!!!!

--- titac_minimax/plotter.py
+++ titac_minimax/plotter.py
@@ -3,10 +3,21 @@ import networkx as nx
 
 class Plotter:
     
-    def plot_tree(self, root_node):
+    def plot_tree(self, root_node: Node):
         '''function to plot the decision tree'''
         graph = nx.DiGraph()
-        return
+        node_labels = {}
+        
+        self.conect_nodes(root_node, graph, node_labels)
+        
+    def conect_nodes(self, root_node: Node, graph, labels: dict):
+        '''aux function that recursively conects nodes in the graph'''
+        labels[id(root_node)] = {"M:{root_node.move}", "S:{root_node.move}"}
+        graph.add_node(id(root_node))
+
+        for child in root_node.children:
+            graph.add_edge(id(root_node), id(child))
+            self.conect_nodes(child, graph, labels)
 
     def print_root_info(self, root_node: Node, depth=0):
         '''recursively traversses the tree printing its information'''
